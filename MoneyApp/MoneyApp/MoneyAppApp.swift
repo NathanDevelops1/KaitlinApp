890397dd5f62930fb70bcb3d815585f68a4f0acf
Nathan Develops <92740411+NathanDevelops1@users.noreply.github.com>
2023-12-03 12:35:41 -0500

    Initial Commit

---
+++ MoneyApp/MoneyApp/MoneyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyAppApp.swift
+//  MoneyApp
+//
+//  Created by Nathan Graff on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MoneyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoneyApp/MoneyApp/NewPage.swift
@@ -0,0 +1,20 @@
+//
+//  NewPage.swift
+//  MoneyApp
+//
+//  Created by Nathan Graff on 12/1/23.
+//
+
+import SwiftUI
+
+struct NewPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewPage_Previews: PreviewProvider {
+    static var previews: some View {
+        NewPage()
+    }
+}
